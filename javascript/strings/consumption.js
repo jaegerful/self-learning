8839943f7ed9f1c1aa5984801d1f1192bf307457
Jaegerly <teamcocohead@gmail.com>
2023-05-17 15:17:30 -0400

    consumption of backslashes for strings in javascript

---
+++ javascript/strings/consumption.js
@@ -0,0 +1,5 @@
+/* backslashes in strings are used to escape special characters. */
+/* if unnecessary backslashes are encountered, they are removed automatically from a string. */
+
+const first = 'today\'s dinner\ was fun\!.'
+console.log(`first: ${first}`)
