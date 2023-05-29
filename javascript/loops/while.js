4aab4289c55c60c28651c65127ceadad1e092d77
Jaegerly <teamcocohead@gmail.com>
2023-05-29 07:50:35 -0400

    while-loop conditions in javascript permit assignments

---
+++ javascript/loops/while.js
@@ -0,0 +1,12 @@
+'use strict'
+
+console.log()
+
+/* assignments are permitted in while loops. */
+
+let result = 0
+
+while ((result = result + 2) <= 6)
+    console.log(`value: ${result}`)
+
+console.log()
